18ed3e693af3d6aacb344a9bbd5e02ebb9b4cf34
David Ji <d.ji@student.vu.nl>
2024-06-27 20:56:06 +0200

    full test coverage coverage

--- tests/unit/core/test_extension_loader.py
+++ tests/unit/core/test_extension_loader.py
@@ -7,18 +7,17 @@ class TestGetTestId(unittest.TestCase):
     def setUp(self):
         self.manager = extension_loader.Manager()
         self.manager.load_plugins("bandit.plugins")
-        # self.assertTrue(False, f'plugins: {self.manager.plugins_by_name}')
-
+        
     def test_get_test_id(self):
         self.manager.load_blacklists("bandit.blacklists")
         self.manager.load_formatters("bandit.formatters")
         self.manager.load_plugins("bandit.plugins")
-        # self.assertTrue(False, f'test id is: {self.manager.get_test_id("B001")}')
-        self.manager.get_test_id("B001")
+        self.assertIs(self.manager.get_test_id("B001"), None, f'failed, test id is: {self.manager.get_test_id("B001")}')
+        
     
     def test_get_test_id2(self):
         self.manager.load_blacklists("bandit.blacklists")
         self.manager.load_formatters("bandit.formatters")
         self.manager.load_plugins("bandit.plugins")
-        # self.assertTrue(False, f'test id is: {self.manager.get_test_id("any_other_function_with_shell_equals_true")}')
-        self.manager.get_test_id("any_other_function_with_shell_equals_true")
+        self.assertIs(self.manager.get_test_id("any_other_function_with_shell_equals_true"), "B604", f'test id is: {self.manager.get_test_id("any_other_function_with_shell_equals_true")}')
+        
